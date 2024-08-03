c2e0ccfd4cf6fe9ed28d4886f88a5b628e4083f4
aarvy-rv <rajvaibhavcs22@gmail.com>
2024-08-03 09:54:07 +0530

    V3: Mutiple go routine with same channels but looping through different values of channels and closing the one which runs the longest

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func slowGreet(done chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Slow Greet invoked!")
 	done <- true
+	close(done) // this will work if we know beforehand which method will take the longest time
+	// if we do not close the channel error occurs as looping through the values of channel
+	// eventually leading empty channel therefore throws error
 }
 
 func greetTwo(done chan bool) {
@@ -28,24 +31,18 @@ func greetThree(done chan bool) {
 
 func main() {
 
-	//done := make(chan bool) // We can use one channel for multiple go routines, however we need to read the channels
+	done := make(chan bool) // We can use one channel for multiple go routines, however we need to read the channels
 	// for all the number of times go routines is invokek i.e. 4 times in this example to avoid the kind of race condition
 
-	dones := make([]chan bool, 4)
+	go greetOne(done)
 
-	dones[0] = make(chan bool)
-	go greetOne(dones[0])
+	go slowGreet(done)
 
-	dones[1] = make(chan bool)
-	go slowGreet(dones[1])
+	go greetTwo(done)
 
-	dones[2] = make(chan bool)
-	go greetTwo(dones[2])
+	go greetThree(done)
 
-	dones[3] = make(chan bool)
-	go greetThree(dones[3])
-
-	for _, done := range dones {
+	for range done {
 		<-done
 	}
 
